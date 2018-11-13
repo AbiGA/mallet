ee5169d845a6166dcf3c7d7711d82704263c1a2b
AbiGA <abhishekjoshi724@gmail.com>
2018-11-13 11:17:52 +0530

    favicon added, minor ui updates

--- src/app/pages/productsinside/productsinside.component.ts
+++ src/app/pages/productsinside/productsinside.component.ts
@@ -11,6 +11,14 @@ export class ProductsinsideComponent implements OnInit {
   constructor(private router: Router) {
    }
 
+  image1 = "../../../assets/images/productinside/pie 6.png";
+  image2 = "../../../assets/images/productinside/pie 2.png";
+  image3 = "../../../assets/images/productinside/pie 7.png";
+  mainimage = "../../../assets/images/productinside/pie 1.png";
+
+  changeMainImage(image: any){
+    this.mainimage = image;
+  }
    scroll(el) {
     el.scrollIntoView();
   }
